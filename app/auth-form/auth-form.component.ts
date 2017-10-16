c2a592d3927811fee871bb00572125bc5a10a53c
Adrian Rincon Lopez <adrianrl9441@gmail.com>
2017-10-16 23:27:29 +0200

    Querying a ViewChild element, add classes, add attr acceding to the native element

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, Output, /*ViewChild*/ ViewChildren, AfterViewInit, EventEmitter, /*ContentChild*/ ContentChildren,  QueryList, AfterContentInit } from '@angular/core';
+import { Component, ChangeDetectorRef, Output, ViewChild, ElementRef, ViewChildren, AfterViewInit, EventEmitter, /*ContentChild*/ ContentChildren,  QueryList, AfterContentInit } from '@angular/core';
 
 import { AuthRememberComponent } from './auth-remember.component';
 import { AuthMessageComponent } from './auth-message.component';
@@ -7,6 +7,7 @@ import { User } from './auth-form.interface';
 
 @Component({
   selector: 'auth-form',
+  styles: ['.email{ border-color: #9f72e6;'],
   template: `
     <div>
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
@@ -14,7 +15,7 @@ import { User } from './auth-form.interface';
         <!-- We can use an #id or .class to select an element in ng-content -->
         <label>
           Email address
-          <input type="email" name="email" ngModel>
+          <input type="email" name="email" ngModel #email>
         </label>
         <label>
           Password
@@ -23,10 +24,11 @@ import { User } from './auth-form.interface';
         <ng-content select="auth-remember"></ng-content>
         <auth-message [style.display]="(showMessage ? 'inherit' : 'none')">
         </auth-message>
+        <!-- ViewChildren example
         <auth-message [style.display]="(showMessage ? 'inherit' : 'none')">
         </auth-message>
         <auth-message [style.display]="(showMessage ? 'inherit' : 'none')">
-        </auth-message>
+        </auth-message>-->
         <ng-content select="button"></ng-content>
       </form>
     </div>
@@ -36,7 +38,10 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
   showMessage: boolean;
 
+  @ViewChild('email') email: ElementRef;
+
   @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent>;
+ // @ViewChildr(AuthMessageComponent) message: AuthMessageComponent;
 
   /* Control 1 child of the parent 
   @ContentChild(AuthRememberComponent) remember: AuthRememberComponent; */
@@ -50,12 +55,15 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
   constructor(private cd: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
+    this.email.nativeElement.setAttribute('placeholder', 'Enter your email address');
+    this.email.nativeElement.classList.add('email');
+    this.email.nativeElement.focus();
     // Cuando uso viewChildren se modifica en AfterViewInit
     if(this.message) {
       this.message.forEach((message) => {
         message.days = 30;
       });
-      //this.cd.detectChanges();
+      this.cd.detectChanges();
     }
   }
 
